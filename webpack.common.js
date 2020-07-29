52bf9e0b8aba4bad66f4f406726c8e6ccee79877
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-07-29 23:48:41 +0200

    minify HTML, CSS, and JavaScript

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,8 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
   module: {
     rules: [
       {
