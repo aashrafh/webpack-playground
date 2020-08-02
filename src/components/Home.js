e162df862ea74bef9817a4f65fa7924022981d15
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-08-02 20:05:02 +0200

    home behavior

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Layout from "./Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <p>Hello World from React and Webpack</p>
+      <p>
+        <Link to="/dynamic">Click here to navigate to the dynamic page</Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default Home;
